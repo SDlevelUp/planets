b76c394abdd8acc2fca631276635c1b4fbb0e7f3
Sarah Berri <sarahberri@outlook.fr>
2023-07-03 21:13:17 +0200

    sdf

--- src/Navbar/Navbar.jsx
+++ src/Navbar/Navbar.jsx
@@ -1,27 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Navbar.scss';
 import { MdClose, MdDehaze } from 'react-icons/md';
 import { MenuItems } from '../data/MenuItems';
-import { useState } from 'react';
 
 const Navbar = () => {
-
-    let [clicked, setClicked] = useState(false);
-    const change = () => setClicked(!clicked)
+    const [clicked, setClicked] = useState(false);
+    const change = () => setClicked(!clicked);
 
     return (
         <nav className='navbar'>
             <p className='logo'>Planete</p>
-            <div className="menuIcon">
-                {clicked ? <MdClose onClick={change} /> : <MdDehaze onClick={change} />}
+            <div className='menuIcon' onClick={change}>
+                {clicked ? <MdClose /> : <MdDehaze />}
             </div>
-            <ul className={`menu-items ${clicked ? "visible" : ''}`}>
+            <ul className={`menu-items ${clicked ? 'visible' : ''}`}>
                 {MenuItems.map((item, index) => (
-                    <li key={index} className={item.cName}>{item.title}</li>
+                    <li key={index} className={item.cName}>
+                        {item.title}
+                    </li>
                 ))}
             </ul>
         </nav>
-    )
-}
+    );
+};
 
 export default Navbar;
