0db496f465a95177acbc226baa7cbaaa28f947a4
Sarah Berri <sarahberri@outlook.fr>
2023-07-06 01:25:42 +0200

    test layer hover

--- src/FlippableCard.jsx
+++ src/FlippableCard.jsx
@@ -1,13 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './_FlippableCard.module.scss';
 import PlanetsCard from './components/PlanetsCards/PlanetsCards';
+import { CSSTransition } from 'react-transition-group';
+import flipTransitionStyles from './_FlippableCard.module.scss';
 
 const FlippableCard = () => {
+    const [showFront, setShowFront] = useState(true);
     return (
         <div className={styles['flippable-card-container']}>
-            <PlanetsCard />
+            <CSSTransition
+                in={showFront}
+                timeout={300}
+                classNames={flipTransitionStyles['flip']}>
+
+                <PlanetsCard onClick={() => {
+                    setShowFront((v) => !v);
+                }} />
+            </CSSTransition>
         </div>
     );
-};
+}
 
 export default FlippableCard;
--- src/components/PlanetsCards/PlanetsCards.jsx
+++ src/components/PlanetsCards/PlanetsCards.jsx
@@ -1,26 +1,24 @@
-import React from 'react';
+
 import PlanetsData from '../../data/PlanetsData.json';
 import styles from './_PlanetsCards.module.scss';
-
+import "./_FlipTransition.module.scss"
 const PlanetsCard = ({ onClick }) => {
     return (
         <div className={styles['planet-card-container']}>
-            {PlanetsData.map((planet) => {
-                return (
-                    <div key={planet.name} className={styles['planet-card']} onClick={onClick}>
-                        <div className={styles['planet-card-back']}>
-                            <img src={planet.imageUrl} alt={planet.name} className={styles['planet-card-image']} />
-                        </div>
-
-                        <div className={styles['planet-card-front']}>
-                            <h2>{planet.name}</h2>
-                            <p>{planet.definition}</p>
-                            <p>Distance: {planet.distance}</p>
-                            <p>Type: {planet.type}</p>
-                        </div>
+            {PlanetsData.map((planet) => (
+                <div key={planet.name} className={styles['planet-card']} onClick={onClick}>
+                    <div className={`${styles['planet-card-back']} ${styles['layer']}`}>
+                        <h2 className={styles['planet-card-title']}>{planet.name}</h2>
+                        <p className={styles['planet-card-definition']}>{planet.definition}</p>
+                        <p className={styles['planet-card-distance']}>Distance: {planet.distance}</p>
+                        <p className={styles['planet-card-type']}>Type: {planet.type}</p>
                     </div>
-                );
-            })}
+
+                    <div className={styles['planet-card-front']}>
+                        <img src={planet.imageUrl} alt={planet.name} className={styles['planet-card-image']} />
+                    </div>
+                </div>
+            ))}
         </div>
     );
 };
