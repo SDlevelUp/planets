ce3bcf97116d27d60af36c6ed0bc33e495efccb8
Sarah Berri <sarahberri@outlook.fr>
2023-07-06 11:33:11 +0200

    test animation back card

--- src/components/PlanetsCards/PlanetsCards.jsx
+++ src/components/PlanetsCards/PlanetsCards.jsx
@@ -1,17 +1,24 @@
-
+import React from 'react';
 import PlanetsData from '../../data/PlanetsData.json';
 import styles from './_PlanetsCards.module.scss';
-import "./_FlipTransition.module.scss"
+import './_FlipTransition.module.scss';
+import { PiPlanetLight } from 'react-icons/pi';
+
 const PlanetsCard = ({ onClick }) => {
     return (
         <div className={styles['planet-card-container']}>
             {PlanetsData.map((planet) => (
                 <div key={planet.name} className={styles['planet-card']} onClick={onClick}>
                     <div className={styles['planet-card-back']}>
-                        <h2 className={styles['planet-card-title']}>{planet.name}</h2>
-                        <p className={styles['planet-card-definition']}>{planet.definition}</p>
-                        <p className={styles['planet-card-distance']}>Distance: {planet.distance}</p>
-                        <p className={styles['planet-card-type']}>Type: {planet.type}</p>
+                        <div className={styles['icon-bx']}>
+                            <PiPlanetLight size={32} /> {/* Set the desired size here */}
+                        </div>
+                        <div className={styles['planet-card-content']}>
+                            <h2 className={styles['planet-card-title']}>{planet.name}</h2>
+                            <p className={styles['planet-card-definition']}>{planet.definition}</p>
+                            <p className={styles['planet-card-distance']}>Distance: {planet.distance}</p>
+                            <p className={styles['planet-card-type']}>Type: {planet.type}</p>
+                        </div>
                     </div>
 
                     <div className={styles['planet-card-front']}>
