46661aa6a6c0455e3679c3790542e90c95253833
Sarah Berri <sarahberri@outlook.fr>
2023-07-11 00:35:15 +0200

    maj partie galerie

--- src/components/PlanetsCards/PlanetsCards.jsx
+++ src/components/PlanetsCards/PlanetsCards.jsx
@@ -3,16 +3,20 @@ import PlanetsData from '../../data/PlanetsData.json';
 import styles from './_PlanetsCards.module.scss';
 import './_FlipTransition.module.scss';
 import { PiPlanetLight } from 'react-icons/pi';
+import gallery from '../../assets/galleryban.jpg';
 
 const PlanetsCard = ({ onClick }) => {
     return (
         <>
+            <div className={styles['gallery-banner']}>
+                <img src={gallery} alt="bannière galerie" />
+            </div>
             <div className={styles['planet-card-presentation']}>
-                <h1>Présentation de la galerie</h1>
+                <h1>Presentation de la galerie</h1>
                 <p>Dans cette partie, vous trouverez les planètes que vous connaissez:
-                    Mars, Vénus, Mercure, et celle où nous vivons sous forme de cartes,
+                    Mars, Vénus, Mercure, ... et celle où nous vivons ( La Terre ) sous forme de cartes,
                     qui au passage de la souris s'animeront de sorte que vous puissiez
-                    avoir accès aux informations utiles pour chaque planètes </p>
+                    avoir accès aux informations utiles pour chaque planète. Essayez, et vous verrez ! </p>
             </div>
             <div className={styles['planet-card-container']}>
                 {PlanetsData.map((planet) => (
