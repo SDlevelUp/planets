3fdd2f82b5b519a50ca376038c0f0554c7eb5720
Sarah Berri <sarahberri@outlook.fr>
2023-07-15 17:04:03 +0200

    maj menu

--- src/menuItems.js
+++ src/menuItems.js
@@ -1,19 +1,13 @@
 export const menuItems = [
     { title: "Accueil", path: "/" },
-    {
-        title: "Galerie",
-        path: "/galerie",
-        submenu: [
-            { title: "Planetarypix", path: "/galerie" },
-            { title: "Quiz", path: "/galerie/quiz" },
-        ],
-    },
+    { title: "Galerie", path: "/galerie" },
     {
         title: "Astronomie",
         path: "/",
         submenu: [
             { title: "L'Astronium", path: "/astronomie" },
             { title: "Systeme solaire", path: "/astronomie/systemesolaire" },
+            { title: "Quiz", path: "/astronomie/quiz" },
         ],
     },
     { title: "Contact", path: "/contact" },
