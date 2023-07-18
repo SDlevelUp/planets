81a74aaaec584c92fc0acbd53f182703b3411dec
Sarah Berri <sarahberri@outlook.fr>
2023-07-18 21:17:30 +0200

    maj question js

--- src/js/constants.js
+++ src/js/constants.js
@@ -2,7 +2,92 @@ export const quiz = {
     questions: [
         {
             question:
-                ""
-        }
+                "Quelle est la planète la plus proche du système soleil ?",
+            choices: [
+                "Neptune",
+                "Mars",
+                "Mercure",
+                "Pluton",
+            ],
+            type: "MCQs",
+            correctAnsswer: "Mercure",
+        },
+        {
+            question:
+                "De quelle couleur est la planète Uranus ?",
+            choices: [
+                "Bleu-vert",
+                "Rouge",
+                "Bleu",
+                "Orange",
+            ],
+            type: "MCQs",
+            correctAnsswer: "Bleu-vert",
+        },
+        {
+            question:
+                "De quelle nature est laplanète Neptune ?",
+            choices: [
+                "Aucun",
+                "Gazeuse",
+                "Aqueuse",
+                "Rocheuse",
+            ],
+            type: "MCQs",
+            correctAnsswer: "Gazeuse",
+        },
+        {
+            question:
+                "Quelle est la planète la plus proche du soleil ?",
+            choices: [
+                "Neptune",
+                "Mars",
+                "Mercure",
+                "Pluton",
+            ],
+            type: "MCQs",
+            correctAnsswer: "Mercure",
+        },
+        {
+            question:
+                "Saturne est célèbre pour sa/ses?",
+            choices: [
+                "Température",
+                "Etoiles",
+                "Anneaux spectaculaires",
+                "Distance au soleil",
+            ],
+            type: "MCQs",
+            correctAnsswer: "Ses anneaux spectaculaires",
+        },
+        {
+            question:
+                "A combien équivaut 1 ua (unité astronomique) ?",
+            choices: [
+                "178 584 925,15 km",
+                "652 100 581,87 km",
+                "149 597 870,7 km",
+                "425 630 682,77 km",
+            ],
+            type: "MCQs",
+            correctAnsswer: "149 597 870,7 km",
+        },
+        {
+            question:
+                "Quelle est la plus grande planète du système solaire ?",
+            choices: [
+                "Jupiter",
+                "La Terre",
+                "Uranus",
+                "Pluton",
+            ],
+            type: "MCQs",
+            correctAnsswer: "Jupiter",
+        },
+
+
+
+
+
     ]
 }
